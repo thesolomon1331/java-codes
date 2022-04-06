670c963180e5db89a993d8c1baa4527d377277ea
Solomon Raju G <53774710+thesolomon1331@users.noreply.github.com>
2022-04-06 17:30:38 +0530

    Add files via upload

---
+++ LargestNum.java
@@ -0,0 +1,29 @@
+package myprojects;
+import java.util.*;
+public class LargestNum
+{
+	private static final int NULL = 0;
+
+	public static void main(String[] args)
+	{
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter 3 numbers: ");
+		int x = s.nextInt();
+		int y= s.nextInt();
+		int z= s.nextInt();
+		int result=NULL;
+		if(x>y && x>z)
+		{
+			result=x;
+		}
+		else if (y>=z)
+		{
+			result = y;
+		}
+		else
+		{
+			result= z;
+		}
+		System.out.print(result);
+	}
+}
